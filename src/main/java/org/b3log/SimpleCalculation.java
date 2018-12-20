28c97bf28b0a324fea1b62837d94b9ca0c1c4b0a
yaoguipingboy <45711354+yaoguipingboy@users.noreply.github.com>
2018-12-20 22:06:00 +0800

    Update SimpleCalculation.java

--- src/main/java/org/b3log/SimpleCalculation.java
+++ src/main/java/org/b3log/SimpleCalculation.java
@@ -2,11 +2,11 @@ package org.b3log;
 
 public class SimpleCalculation {
     public int Add(int a,int b){        
-        return (a+b);        
+        return a+b;        
     }
 
     public int Subtration(int a,int b){
-        return(a-b);
+        return a-b;
     }
     
 }
