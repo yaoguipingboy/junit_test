04947c417f8c0f1199974b83b97380bb7cca8ad4
yaoguipingboy <45711354+yaoguipingboy@users.noreply.github.com>
2018-12-20 22:05:22 +0800

    Update ComplexCalculation.java

--- src/main/java/org/b3log/ComplexCalculation.java
+++ src/main/java/org/b3log/ComplexCalculation.java
@@ -2,10 +2,10 @@ package org.b3log;
 
 public class ComplexCalculation {
     public int Division(int a,int b){        
-        return (a/b);        
+        return a/b;        
     }
 
     public int Multiply(int a,int b){        
-        return (a*b);        
+        return a*b;        
     }
 }
