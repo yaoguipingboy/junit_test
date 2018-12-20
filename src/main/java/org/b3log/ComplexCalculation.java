69ee0feb95296d330aa510ab2a2065f8f00b7224
yaoguipingboy <45711354+yaoguipingboy@users.noreply.github.com>
2018-12-20 21:42:27 +0800

    Create ComplexCalculation.java

---
+++ src/main/java/org/b3log/ComplexCalculation.java
@@ -0,0 +1,11 @@
+package org.b3log;
+
+public class ComplexCalculation {
+    public int Division(int a,int b){        
+        return (a/b);        
+    }
+
+    public int Multiply(int a,int b){        
+        return (a*b);        
+    }
+}
